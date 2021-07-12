c2f86c0019f4af5430f252fa7a6362eb85a0361d
Brian <brianrey3@protonmail.com>
2021-07-12 11:10:14 -0300

    Initial setup

---
+++ src/helpers/messages.ts
@@ -0,0 +1,8 @@
+import "colors";
+
+export const showMenu = () => {
+  console.clear();
+  console.log("====================================");
+  console.log("Select an option");
+  console.log("====================================");
+};
---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import "colors";
+import "module-alias/register";
+import { showMenu } from "@helpers/messages";
+
+const main = async () => {
+  console.log("====================================");
+  console.log("Hello world");
+  console.log("====================================");
+};
+
+main();
